fe2f564d84db68c6900df8037f9a5f439cf9209c
christianlacuesta <lacuestachristian@yahoo.com>
2022-09-07 09:35:34 +0300

    updated config service

--- client/src/app/services/app-config.service.ts
+++ client/src/app/services/app-config.service.ts
@@ -1,10 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AppLoginElementModel } from '../models/app-models/app-login-element.model';
 
 @Injectable({ providedIn: 'root' })
 export class ConfigService {
 
-    loginPageElements = {
-        // appLogo
+    loginPageElements: AppLoginElementModel = {
+        logo: null,
+        title: {
+            name: '',
+            label: '',
+            label2: '',
+            code: ''
+        },
+        welcomeMessage: {
+            name: '',
+            label: '',
+            label2: '',
+            code: ''
+        },
+        instructionMessage: {
+            name: '',
+            label: '',
+            label2: '',
+            code: ''
+        },
+        buttonLabel: {
+            name: '',
+            label: '',
+            label2: '',
+            code: ''
+        }, 
     };
 
     constructor() { }
